dc8645de8be96ecfb8beda18ec41f1b23c193efa
ranaali73 <ranaalizeeshan73@gmail.com>
2024-03-17 01:21:55 +0500

    Q2-First-Assignment

---
+++ raz-next-project/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"sOOORPC78MsA8RrtzPVKsz5Yc6NjgwRnqRdgnalKHWs=\"\n}"
---
+++ raz-next-project/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <h1 style={{ fontFamily: 'Arial, sans-serif', color: 'blue', textAlign: 'center' }}>
+        السَّلامُ عَلَيْكُم ورَحْمَةُ اللّٰہِ وَبَرَكاتُهُ
+      </h1>
+      <p style={{ fontFamily: 'Arial, sans-serif', fontSize: '18px', textAlign: 'center' }}>
+        My name is Rana Ali Zeeshan and I'm a software engineer.
+      </p>
+    </>
+  );
+}
